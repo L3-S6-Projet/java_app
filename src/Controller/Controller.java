71e1c2519af7d069c2b8ebbf0fc5ab3fb14a35e7
damienmarrou <44019360+damienmarrou@users.noreply.github.com>
2020-04-21 10:29:49 +0200

    begin Login Model

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,4 +1,24 @@
 package Controller;
 
-public class Controller {
+import Model.Login;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Controller extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        stage.setTitle("Calendar");
+        Scene scene = new Scene(root, 1000, 700);
+        stage.setScene(scene);
+        new Login(root);
+        stage.show();
+    }
 }
