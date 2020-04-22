dcbac50f28de430232ba415d76816fa7c8aa7f16
damienmarrou <44019360+damienmarrou@users.noreply.github.com>
2020-04-22 13:23:00 +0200

    Begin LoginView.fxml

--- src/main/java/jscolendar/components/Login.java
+++ src/main/java/jscolendar/components/Login.java
@@ -1,9 +1,30 @@
 package jscolendar.components;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import jscolendar.util.FXUtil;
 
+
 public class Login extends VBox {
+
+  public HBox body;
+  public VBox left;
+  public ImageView picture;
+  public Text title;
+  public Text subtitle;
+  public TextField id;
+  public TextField password;
+  public VBox field;
+  @FXML
+  Button connexion;
+  @FXML
+  Text copyRigth;
+
 
   public Login () {
     FXUtil.loadFXML("/fxml/LoginView.fxml", this, this);
