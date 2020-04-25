e59365c7d602b1aa18dd0d7331ddbd67cf34ec05
damienmarrou <44019360+damienmarrou@users.noreply.github.com>
2020-04-25 14:49:37 +0200

    add all field

--- src/main/java/jscolendar/components/popup/admin/Edition.java
+++ src/main/java/jscolendar/components/popup/admin/Edition.java
@@ -1,9 +1,11 @@
 package jscolendar.components.popup.admin;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
@@ -15,7 +17,7 @@ import java.util.ArrayList;
 
 public class Edition extends VBox {
 
-  private final ArrayList<Label> emptyFiledLabel = new ArrayList<Label>();
+  private final ArrayList<Label> emptyFiledLabel = new ArrayList<>();
 
 
   public VBox body;
@@ -36,6 +38,22 @@ public class Edition extends VBox {
   public JFXTextField nomInput;
   public TextField emptyNomInput;
 
+  public VBox classe;
+  public Label emptyFieldClasse;
+  public StackPane classeLayout;
+  public Label classeLabel;
+  public JFXTextField classeInput;
+  public TextField emptyClasseInupt;
+
+
+  public VBox mdp;
+  public StackPane PWDLayout;
+  public Label mdpLabel;
+  public JFXPasswordField mdpInput;
+  public JFXTextField mdpShowInput;
+  public TextField emptyMdpInput;
+  public Button show;
+
 
   public Label msg;
   public HBox buttons;
@@ -51,18 +69,17 @@ public class Edition extends VBox {
 
     emptyFiledLabel.add(emptyFieldPrenom);
     emptyFiledLabel.add(emptyFieldNom);
+    emptyFiledLabel.add(emptyFieldClasse);
     title.setText("Edition");
     title.setPadding(new Insets(21, 24, 27, 24));
 
     emptyPrenomInput.setEditable(false);
     emptyNomInput.setEditable(false);
 
-    prenomLabel.setVisible(true);
     prenomLabel.setPadding(new Insets(-40, 0, 0, -150));
 
-    emptyPrenomInput.setVisible(false);
 
-    nomLabel.setPadding(new Insets(-60, 0, 0, -150));
+    nomLabel.setPadding(new Insets(-40, 0, 0, -150));
     nomLabel.setVisible(false);
 
     msg.setText("A remplir seulement pour changer de mot de passe.");
@@ -70,7 +87,8 @@ public class Edition extends VBox {
     buttons.setPadding(new Insets(0, 16, 8, 80));
     annuler.setText("ANNULER");
     save.setText("SAVE");
-    hideEmptyFieldMessage();
+
+    fieldPrenom();
   }
 
   @FXML
@@ -79,7 +97,7 @@ public class Edition extends VBox {
 
     prenomLabel.setVisible(true);
     prenomInput.setVisible(true);
-    emptyFieldPrenom.setVisible(false);
+    emptyPrenomInput.setVisible(false);
 
     if (nomInput.getText().isEmpty()) {
       nomLabel.setVisible(false);
@@ -94,7 +112,8 @@ public class Edition extends VBox {
 
     nomLabel.setVisible(true);
     nomInput.setVisible(true);
-    emptyFieldNom.setVisible(false);
+    emptyNomInput.setVisible(false);
+
 
     if (prenomInput.getText().isEmpty()) {
       prenomLabel.setVisible(false);
