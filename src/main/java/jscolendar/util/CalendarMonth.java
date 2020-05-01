cab28d79327a9223967172730d0dd44446ec6205
damienmarrou <44019360+damienmarrou@users.noreply.github.com>
2020-05-01 15:00:30 +0200

    refactor

--- src/main/java/jscolendar/util/CalendarMonth.java
+++ src/main/java/jscolendar/util/CalendarMonth.java
@@ -153,49 +153,7 @@ public class CalendarMonth extends StackPane {
   private int flatIndex(int x, int y) {
     return y * 7 + x;
   }
-/*
-  private void initCells() {
-    for (int x = 0; x < 7; x++) {
-      for (int y = 0; y < 5; y++) {
-        cells[x][y] = 0;
-      }
-    }
-  }*/
 
-
-  /*private void addElement(String name, String salle, int x, int y) {
-    cells[x][y]++;
-    if (cells[x][y] == 1) {
-      canvas.getGraphicsContext2D().setFill(Color.WHITESMOKE);
-      canvas.getGraphicsContext2D().setStroke(Color.BLACK);
-      canvas.getGraphicsContext2D().fillRoundRect(xOrigin + x * 214, yOrigin + 46 + y * 175, 214, 50, 20, 20);
-      canvas.getGraphicsContext2D().strokeRoundRect(xOrigin + x * 214, yOrigin + 46 + y * 175, 214, 50, 20, 20);
-      canvas.getGraphicsContext2D().setFontSmoothingType(FontSmoothingType.GRAY);
-      canvas.getGraphicsContext2D().setFill(Color.BLACK);
-      canvas.getGraphicsContext2D().setFont(new Font("Roboto Light", 20));
-      canvas.getGraphicsContext2D().fillText(name, xOrigin + x * 214, yOrigin + 46 + y * 175 + 20);
-      canvas.getGraphicsContext2D().setFill(Color.GRAY);
-      canvas.getGraphicsContext2D().setFont(new Font("Roboto Light", 16));
-      canvas.getGraphicsContext2D().fillText(salle, xOrigin + x * 214, yOrigin + 46 + y * 175 + 36, 214);
-    } else if (cells[x][y] == 2) {
-      canvas.getGraphicsContext2D().setFill(Color.WHITESMOKE);
-      canvas.getGraphicsContext2D().setStroke(Color.BLACK);
-      canvas.getGraphicsContext2D().fillRoundRect(xOrigin + x * 214, yOrigin + 46 + y * 175 + 55, 214, 50, 20, 20);
-      canvas.getGraphicsContext2D().strokeRoundRect(xOrigin + x * 214, yOrigin + 46 + y * 175 + 55, 214, 50, 20, 20);
-      canvas.getGraphicsContext2D().setFontSmoothingType(FontSmoothingType.GRAY);
-      canvas.getGraphicsContext2D().setFill(Color.BLACK);
-      canvas.getGraphicsContext2D().setFont(new Font("Roboto Light", 20));
-      canvas.getGraphicsContext2D().fillText(name, xOrigin + x * 214, yOrigin + 46 + y * 175 + 20 + 55);
-      canvas.getGraphicsContext2D().setFill(Color.GRAY);
-      canvas.getGraphicsContext2D().setFont(new Font("Roboto Light", 16));
-      canvas.getGraphicsContext2D().fillText(salle, xOrigin + x * 214, yOrigin + 46 + y * 175 + 36 + 55, 214);
-    } else if (cells[x][y] >= 3) {
-      canvas.getGraphicsContext2D().setFill(Color.GRAY);
-      canvas.getGraphicsContext2D().setFont(new Font("Roboto Light", 14));
-      canvas.getGraphicsContext2D().clearRect(xOrigin + x * 250, yOrigin + 46 + y * 175 + 20 + 55 + 50 - 14, 112, 15);
-      canvas.getGraphicsContext2D().fillText(cells[x][y] - 2 + " de plus", xOrigin + x * 250, yOrigin + 46 + y * 175 + 20 + 55 + 50, 112);
-    }
-  }*/
 
   @FXML
   private void selectCalendarType() {
