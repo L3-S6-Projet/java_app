be99f65c4ba9c1c023a6a45354bf859516fc5857
damienmarrou <44019360+damienmarrou@users.noreply.github.com>
2020-05-08 16:46:13 +0200

    add CreateRoom pop

---
+++ src/main/java/jscolendar/components/popup/CreateRoom.java
@@ -0,0 +1,33 @@
+package jscolendar.components.popup;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+
+public class CreateRoom extends StackPane {
+
+  @FXML
+  private JFXTextField name, capacity;
+  @FXML
+  private Label errorLabel;
+  @FXML
+  private JFXButton cancel, save;
+
+  @FXML
+  public void initialize () {
+
+  }
+
+  @FXML
+  private void onCancel () {
+
+  }
+
+  @FXML
+  private void onCreate () {
+
+  }
+
+}
