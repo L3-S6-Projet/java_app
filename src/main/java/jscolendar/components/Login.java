07dabd7c11bbb0ea2768c7c808289edcf7851dca
Nicolas Bourras <dev@nicolasb.fr>
2020-05-08 09:48:43 +0200

    Trial and error with the API

--- src/main/java/jscolendar/components/Login.java
+++ src/main/java/jscolendar/components/Login.java
@@ -1,14 +1,27 @@
 package jscolendar.components;
 
+import com.jfoenix.controls.JFXTextArea;
+import io.swagger.client.ApiCallback;
+import io.swagger.client.ApiException;
+import io.swagger.client.api.AuthApi;
+import io.swagger.client.model.LoginRequest;
+import io.swagger.client.model.SuccessfulLoginResponse;
+import javafx.application.Platform;
+import javafx.scene.control.Label;
 import jscolendar.router.AppRouter;
 
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import jscolendar.util.APIErrorUtil;
+
+import java.util.List;
+import java.util.Map;
 
 public class Login {
   @FXML private JFXTextField usernameField, accessiblePassword;
   @FXML private JFXPasswordField passwordField;
+  @FXML private Label errorLabel;
 
   @FXML public void initialize () {
     accessiblePassword.textProperty().bindBidirectional(passwordField.textProperty());
@@ -36,8 +49,43 @@ public class Login {
   @FXML
   private void onSubmit () {
     if (!usernameField.validate() || !passwordField.validate()) return;
-
-    // @TODO
-    AppRouter.goTo("/main", "admin");
+    doLogin(usernameField.getText(), usernameField.getText());
   }
+
+  private void doLogin(String username, String password) {
+    AuthApi apiInstance = new AuthApi();
+    LoginRequest request = new LoginRequest();
+
+    request.setUsername(username);
+    request.setPassword(password);
+
+    try {
+      apiInstance.loginAsync(request, new ApiCallback<>() {
+        @Override
+        public void onFailure(ApiException e, int i, Map<String, List<String>> map) {
+          Platform.runLater(() -> errorLabel.setText(APIErrorUtil.getErrorMessage(apiInstance.getApiClient(), e)));
+        }
+
+        @Override
+        public void onSuccess(SuccessfulLoginResponse successfulLoginResponse, int i, Map<String, List<String>> map) {
+          // TODO: authenticate user
+          AppRouter.goTo("/main", "admin");
+        }
+
+        @Override
+        public void onUploadProgress(long l, long l1, boolean b) {
+
+        }
+
+        @Override
+        public void onDownloadProgress(long l, long l1, boolean b) {
+
+        }
+      });
+    } catch (ApiException e) {
+      System.err.println("Exception when calling AuthApi#login");
+      e.printStackTrace();
+    }
+  }
+
 }
---
+++ src/main/java/jscolendar/util/APIErrorUtil.java
@@ -0,0 +1,62 @@
+package jscolendar.util;
+
+import com.google.gson.annotations.SerializedName;
+import io.swagger.client.ApiClient;
+import io.swagger.client.ApiException;
+import io.swagger.client.JSON;
+import io.swagger.client.model.ErrorResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class APIErrorUtil {
+
+  // TODO: add proper translations
+  private static Map<ErrorResponse.CodeEnum, String> messages = Map.ofEntries(
+    Map.entry(ErrorResponse.CodeEnum.INVALIDCREDENTIALS, "InvalidCredentials"),
+    Map.entry(ErrorResponse.CodeEnum.INSUFFICIENTAUTHORIZATION, "InsufficientAuthorization"),
+    Map.entry(ErrorResponse.CodeEnum.MALFORMEDDATA, "MalformedData"),
+    Map.entry(ErrorResponse.CodeEnum.INVALIDOLDPASSWORD, "InvalidOldPassword"),
+    Map.entry(ErrorResponse.CodeEnum.PASSWORDTOOSIMPLE, "PasswordTooSimple"),
+    Map.entry(ErrorResponse.CodeEnum.INVALIDEMAIL, "InvalidEmail"),
+    Map.entry(ErrorResponse.CodeEnum.INVALIDPHONENUMBER, "InvalidPhoneNumber"),
+    Map.entry(ErrorResponse.CodeEnum.INVALIDRANK, "InvalidRank"),
+    Map.entry(ErrorResponse.CodeEnum.INVALIDID, "InvalidID"),
+    Map.entry(ErrorResponse.CodeEnum.INVALIDCAPACITY, "InvalidCapacity"),
+    Map.entry(ErrorResponse.CodeEnum.TEACHERINCHARGE, "TeacherInCharge"),
+    Map.entry(ErrorResponse.CodeEnum.CLASSROOMUSED, "ClassroomUsed"),
+    Map.entry(ErrorResponse.CodeEnum.INVALIDLEVEL, "InvalidLevel"),
+    Map.entry(ErrorResponse.CodeEnum.CLASSUSED, "ClassUsed"),
+    Map.entry(ErrorResponse.CodeEnum.STUDENTINCLASS, "StudentInClass"),
+    Map.entry(ErrorResponse.CodeEnum.SUBJECTUSED, "SubjectUsed"),
+    Map.entry(ErrorResponse.CodeEnum.TEACHERNOTINCHARGE, "TeacherNotInCharge"),
+    Map.entry(ErrorResponse.CodeEnum.LASTTEACHERINSUBJECT, "LastTeacherInSubject"),
+    Map.entry(ErrorResponse.CodeEnum.LASTGROUPINSUBJECT, "LastGroupInSubject"),
+    Map.entry(ErrorResponse.CodeEnum.CLASSROOMALREADYOCCUPIED, "ClassroomAlreadyOccupied"),
+    Map.entry(ErrorResponse.CodeEnum.CLASSORGROUPALREADYOCCUPIED, "ClassOrGroupAlreadyOccupied"),
+    Map.entry(ErrorResponse.CodeEnum.INVALIDOCCUPANCYTYPE, "InvalidOccupancyType"),
+    Map.entry(ErrorResponse.CodeEnum.ENDBEFORESTART, "EndBeforeStart"),
+    Map.entry(ErrorResponse.CodeEnum.TEACHERDOESNOTTEACH, "TeacherDoesNotTeach"),
+    Map.entry(ErrorResponse.CodeEnum.ILLEGALOCCUPANCYTYPE, "IllegalOccupancyType"),
+    Map.entry(ErrorResponse.CodeEnum.TEACHERALREADYOCCUPIED, "TeacherAlreadyOccupied"),
+    Map.entry(ErrorResponse.CodeEnum.UNKNOWN, "Unknown")
+  );
+
+  public static String getErrorMessage(ApiClient client, ApiException e) {
+    // Local / Network error
+    if (e.getResponseBody() == null) {
+      return e.getLocalizedMessage();
+    }
+
+    JSON json = client.getJSON();
+
+    // Try to parse the error code out of the response
+    try {
+      ErrorResponse parsed = json.deserialize(e.getResponseBody(), ErrorResponse.class);
+      return messages.getOrDefault(parsed.getCode(), messages.get(ErrorResponse.CodeEnum.UNKNOWN));
+    } catch (Exception exc) {
+      return messages.get(ErrorResponse.CodeEnum.UNKNOWN);
+    }
+  }
+
+}
