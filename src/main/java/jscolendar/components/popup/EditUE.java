55242c3c4e1c6209cb017e590e19f37930dfc1d0
damienmarrou <44019360+damienmarrou@users.noreply.github.com>
2020-05-08 17:08:18 +0200

    add EditUE pop

---
+++ src/main/java/jscolendar/components/popup/EditUE.java
@@ -0,0 +1,32 @@
+package jscolendar.components.popup;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+
+public class EditUE extends StackPane {
+
+  @FXML
+  private JFXTextField name, promo, responsibleTeacher;
+  @FXML
+  private Label errorLabel;
+  @FXML
+  private JFXButton cancel, save;
+
+  @FXML
+  public void initialize () {
+
+  }
+
+  @FXML
+  private void onCancel () {
+
+  }
+
+  @FXML
+  private void onCreate () {
+
+  }
+}
