42d1e1363b77fb70362ba3968c7f592695833523
tom <tom@jmenbalance.fr>
2020-05-11 08:33:27 +0200

    add application modal : event-based approach

--- src/main/java/jscolendar/components/Main.java
+++ src/main/java/jscolendar/components/Main.java
@@ -1,10 +1,13 @@
 package jscolendar.components;
 
+import com.jfoenix.controls.JFXDialog;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import jscolendar.UserSession;
+import jscolendar.events.ModalEvent;
 import jscolendar.models.Nav;
 import jscolendar.router.AppRouter;
 import jscolendar.router.ContentManageable;
@@ -13,10 +16,21 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Main implements Initializable {
+  @FXML private StackPane root;
   @FXML private VBox viewContainer;
+  private final JFXDialog modal;
+
+  public Main () {
+    this.modal = new JFXDialog();
+    this.modal.setCacheContainer(true);
+    this.modal.setOverlayClose(false);
+  }
 
   @Override
   public void initialize (URL location, ResourceBundle resources) {
+    root.addEventHandler(ModalEvent.OPEN, this::onOpenModalRequest);
+    root.addEventHandler(ModalEvent.CLOSE, event -> modal.close());
+
     AppRouter.bind("main", new ContentManageable() {
       private final VBox container = viewContainer;
 
@@ -29,5 +43,10 @@ public class Main implements Initializable {
     Nav.create()
       .filter(Nav.visibilityFilter(UserSession.getInstance().getUser().getKind()))
       .forEach(navElement -> AppRouter.when(navElement.linkTo, navElement.fxml));
+  }
+
+  private void onOpenModalRequest (ModalEvent event) {
+    modal.setContent(event.getContent());
+    modal.show(root);
   }
 }
