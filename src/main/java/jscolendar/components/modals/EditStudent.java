03306a83d3ffba6b2938f51c0c1675f12c2ce3cd
damienmarrou <44019360+damienmarrou@users.noreply.github.com>
2020-05-14 10:42:49 +0200

    all edit/creat popup can be show

--- src/main/java/jscolendar/components/modals/EditStudent.java
+++ src/main/java/jscolendar/components/modals/EditStudent.java
@@ -1,6 +1,31 @@
 package jscolendar.components.modals;
 
+import javafx.fxml.FXML;
 import javafx.scene.layout.StackPane;
+import jscolendar.events.ModalEvent;
+import jscolendar.util.FXUtil;
+import jscolendar.util.I18n;
 
 public class EditStudent extends StackPane {
+
+  public EditStudent () {
+    FXUtil.loadFXML("/fxml/modals/EditStudent.fxml", this, this, I18n.getBundle());
+  }
+
+  @FXML
+  public void initialize () {
+
+  }
+
+  @FXML
+  private void onCancel () {
+    this.fireEvent(
+      new ModalEvent(ModalEvent.CLOSE, new CreateTeacher())
+    );
+  }
+
+  @FXML
+  private void onCreate () {
+
+  }
 }
--- src/main/java/jscolendar/components/modals/EditTeacher.java
+++ src/main/java/jscolendar/components/modals/EditTeacher.java
@@ -1,8 +1,31 @@
 package jscolendar.components.modals;
 
+import javafx.fxml.FXML;
 import javafx.scene.layout.StackPane;
+import jscolendar.events.ModalEvent;
+import jscolendar.util.FXUtil;
+import jscolendar.util.I18n;
 
 public class EditTeacher extends StackPane {
+  public EditTeacher () {
+    FXUtil.loadFXML("/fxml/modals/EditTeacher.fxml", this, this, I18n.getBundle());
+  }
 
+  @FXML
+  public void initialize () {
+
+  }
+
+  @FXML
+  private void onCancel () {
+    this.fireEvent(
+      new ModalEvent(ModalEvent.CLOSE, new CreateTeacher())
+    );
+  }
+
+  @FXML
+  private void onCreate () {
+
+  }
 
 }
