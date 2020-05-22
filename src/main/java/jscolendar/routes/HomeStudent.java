8fe69386aa87f80863f5c63a90943ccbff975d1d
damienmarrou <damien.marrou@etu.univ-amu.fr>
2020-05-22 12:59:44 +0200

    header of HomeStudent

--- src/main/java/jscolendar/routes/HomeStudent.java
+++ src/main/java/jscolendar/routes/HomeStudent.java
@@ -1,8 +1,13 @@
 package jscolendar.routes;
 
 import com.calendarfx.view.CalendarView;
+import com.jfoenix.controls.JFXListView;
+import io.swagger.client.ApiException;
+import io.swagger.client.api.RolestudentApi;
 import io.swagger.client.api.SubjectsApi;
 import io.swagger.client.model.Occupancies;
+import io.swagger.client.model.OccupanciesDays;
+import io.swagger.client.model.OccupanciesOccupancies;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
@@ -15,27 +20,31 @@ import jscolendar.models.CalendarDataManager;
 import jscolendar.util.FXApiService;
 import jscolendar.util.I18n;
 
+import java.sql.Timestamp;
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class HomeStudent {
 
 
   private final Integer id = 2;//todo set id value
   @FXML
-  private VBox container, graph, lastModifications;
+  private VBox graph;
   @FXML
   private Label header, graphText;
   @FXML
   private HBox body;
+  @FXML
+  private JFXListView<VBox> linksContent, modificationsContent, contactContent;
 
   @FXML
   public void initialize() {
     ///profile/last-occupancies-modifications
-    String headerContent = I18n.get("home.student.header.first") + " " + "->nb<-" + I18n.get("home.student.header.minutes") + " " +
-      I18n.get("home.student.header.en") + "->la salle<-" +
-      I18n.get("home.student.header.de") + "->beginHour<-" + I18n.get("home.student.header.a") +
-      "->endHour<-" + "\n" + I18n.get("home.student.header.second") + "->nom du cours<-" +
-      I18n.get(("home.student.header.avec")) + "->Nom du prof<-";
-    header.setText(headerContent);
-    header.setWrapText(true);
+    setHeader();
+
+
     FXApiService<Pair<Integer, Integer>, Occupancies> service = null;
     var subjectApi = new SubjectsApi();
     service = new FXApiService<>(request ->
@@ -52,4 +61,43 @@ public class HomeStudent {
 
 
   }
+
+  private void setHeader() {
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    int start = (int) timestamp.getTime() / 1000;
+    long duration = (7 * 24 * 60 * 60);
+    Integer end = (int) (timestamp.getTime() / 1000 + duration);
+    RolestudentApi apiInstance = new RolestudentApi();
+    OccupanciesOccupancies nextOccupancy = new OccupanciesOccupancies();
+    nextOccupancy.setStart(end);
+    try {
+      Occupancies result = apiInstance.studentsIdOccupanciesGet(id, start, end, 15);
+      for (OccupanciesDays occupanciesDays : result.getDays()) {
+        for (OccupanciesOccupancies occupancies : occupanciesDays.getOccupancies()) {
+          if (occupancies.getStart() == null | occupancies.getStart() < nextOccupancy.getStart()) {
+            nextOccupancy = occupancies;
+          }
+        }
+      }
+    } catch (ApiException e) {
+      System.err.println("Exception when calling RolestudentApi#studentsIdOccupanciesGet");
+      e.printStackTrace();
+    }
+
+    Date remaining = new Date((long) (nextOccupancy.getStart() * 1000) - start);
+    Date hourStart = new Date((long) nextOccupancy.getStart() * 1000);
+    Date hourEnd = new Date((long) nextOccupancy.getEnd() * 1000);
+    SimpleDateFormat sdf = new SimpleDateFormat("kk:mm");
+    sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+    String startTime = sdf.format(hourStart);
+    String endTime = sdf.format(hourEnd);
+    String remainingTime = sdf.format(remaining);
+
+    String headerContent = MessageFormat.format(I18n.get("home.student.header.first"), remainingTime,
+      nextOccupancy.getClassroomName(), startTime, endTime) + '\n' + MessageFormat.format(I18n.get("home.student.header.second")
+      , nextOccupancy.getClassroomName(), nextOccupancy.getTeacherName());
+    header.setText(headerContent);
+    header.setWrapText(true);
+  }
+
 }
