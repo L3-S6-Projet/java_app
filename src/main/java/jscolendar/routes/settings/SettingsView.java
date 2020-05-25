b1e18ba857dbe6d01cb7d478770990d77a30d142
tom <tom@jmenbalance.fr>
2020-05-25 05:47:03 +0200

    update settings page

--- src/main/java/jscolendar/routes/settings/SettingsView.java
+++ src/main/java/jscolendar/routes/settings/SettingsView.java
@@ -5,26 +5,15 @@ import javafx.scene.layout.VBox;
 import jscolendar.events.ModalEvent;
 
 public class SettingsView {
+  @FXML private VBox container;
 
-  @FXML
-  private VBox container;
-/*
-  public SettingsView () {
-    FXUtil.loadFXML("/fxml/Settings.fxml",this,this, I18n.getBundle());
-  }*/
-
-  //todo add css to view
   @FXML
   private void changePassWord () {
-    container.fireEvent(
-      new ModalEvent(ModalEvent.OPEN, new ChangePassWord())
-    );
+    container.fireEvent(new ModalEvent(ModalEvent.OPEN, new ChangePassWord()));
   }
 
   @FXML
   private void resetData () {
-    container.fireEvent(
-      new ModalEvent(ModalEvent.OPEN, new ResetData())
-    );
+    container.fireEvent(new ModalEvent(ModalEvent.OPEN, new ResetData()));
   }
 }
