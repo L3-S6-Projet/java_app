67facca25e6147bbbd71b20f644be9dd0923441b
tom <tom@jmenbalance.fr>
2020-05-26 08:44:42 +0200

    fix settings view (non-privileged users)

--- src/main/java/jscolendar/routes/settings/SettingsView.java
+++ src/main/java/jscolendar/routes/settings/SettingsView.java
@@ -1,11 +1,23 @@
 package jscolendar.routes.settings;
 
+import io.swagger.client.model.Role;
 import javafx.fxml.FXML;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import jscolendar.UserSession;
 import jscolendar.events.ModalEvent;
 
 public class SettingsView {
-  @FXML private VBox container;
+  @FXML private VBox container, itemContainer;
+  @FXML private Separator separator;
+  @FXML private HBox resetData;
+
+  @FXML private void initialize () {
+    if (UserSession.getInstance().getUser().getKind() == Role.ADM) return;
+
+    itemContainer.getChildren().removeAll(separator, resetData);
+  }
 
   @FXML
   private void changePassWord () {
